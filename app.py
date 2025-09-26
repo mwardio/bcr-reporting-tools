82a65cc521e079567e6f06ed860fb5e889b4ff8a
Mark Ward <606910+mwardio@users.noreply.github.com>
2025-09-26 12:11:41 -0500

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,8 @@ import streamlit as st
 pages = [
     st.Page("report_builder.py", title="PS Report Builder"),
     st.Page("tetris.py", title="Ad Hoc Slide Maker"),
-    st.Page("api_helper.py", title="ID Lookup")
+    st.Page("api_helper.py", title="API Helper (ID Lookup)"),
+    st.Page("cat_level_metrics.py", title="Categeory-Level Metrics")
 ]
 
 st.set_page_config(layout="centered", page_icon="img/bw_icon.png")
